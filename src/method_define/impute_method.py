a692959def87df0513aae2e89e01d2046a2923ea
Adelina Garamow <adelina.garamow@outlook.de>
2020-03-02 01:08:04 +0100

    Add imputation functions.

--- src/method_define/impute_method.py
+++ src/method_define/impute_method.py
@@ -1,102 +1,63 @@
-"""Define methods to construct a complete data set on covariates or the outcome
-variable(s) or both.
-
-    *For outcome variable(s) and covariates*
-    impute_kNN(): Imputation of missings using the average kNN method.
-
-    impute_median(): Impute missings by the median of each column,
-    plus/ minus some standard deviation.
-
-"""
-
 import numpy as np
-import pandas as pd
 from sklearn.impute import KNNImputer
-from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
 
-def impute_kNN(df, k):
-    """Impute missing values with the average of the k nearest neighbors based
-    on the covariates from the baseline.
+nan = np.nan
+scaler = StandardScaler()
+imputer_kNN = KNNImputer(n_neighbors=1, missing_values=nan, weights="uniform")
 
-        Args:
 
-            df: Data frame (pd.DataFrame)
-
-            k: Number of neighbors (integer)
-
-        Out:
-
-            df_imputed_kNN: Imputed data set, df_imputed_kNN (pd.DataFrame)
-
-    """
-    imputer = KNNImputer(n_neighbors=k, weights="uniform")
-    df_imputed_kNN = imputer.fit_transform(df)
-
-    return df_imputed_kNN
-
-def impute_median_sd(col, sd_share, sd_fixed, k):
+def impute_msd(df, k, sd_share, sd_fixed, col_name):
     """Impute the missing values with the median, plus/ minus some share of the
     standard deviation of the specific variable.
 
-        Args:
+    Args:
 
-            col: Column/ series of the respective data frame (PandaSeries)
+    df (pd.DataFrame() - data set
+    k (integer) - number of nearest n_neighbors
+    sd_share (integer) - share of variance applied
+    sd_fixed (integer) - additional constant to variance
+    col_name (list) - names of variables which should be imputed
 
-            sd_share: Share of the variance (integer)
+    Returns:
 
-            sd_fixed: Add a constant to the variance (integer)
-
-            k: number of imputations (integer)
-
-        Out:
-
-            col: Column with imputed values (PandaSeries)
+    colmiss_ndarray (nd array) - imputed nd array
 
     """
-    if is_numeric_dtype(col)==True:
+    col_ndarray = df[col_name].values
 
-        col_array = col.values
-        colmiss_nan_placeholder = np.isnan(col_array)
+    col_len = np.arange(np.size(col_ndarray, 1))
+    col_median = np.nanmedian(col_ndarray, axis=0)
+    col_sd = np.nanstd(col_ndarray, axis=0)
 
-        if colmiss_nan_placeholder.any()==True and np.ma.all(colmiss_nan_placeholder)==False:
+    for i in col_len:
+        col_nan_placeholder = np.isnan(col_ndarray[:, i])
+        col_fill = np.random.normal(
+            col_median[i],
+            col_sd[i] * sd_share + sd_fixed,
+            [col_nan_placeholder.sum(), k],
+        ).mean(axis=1)
+        col_ndarray[:, i][col_nan_placeholder] = col_fill
 
-            colmiss_median = np.nanmedian(col_array, axis=0)
-            colmiss_sd = np.nanstd(col_array, axis=0)
-
-            colmiss_fill = np.random.normal(colmiss_median, colmiss_sd*sd_share+sd_fixed, [colmiss_nan_placeholder.sum(), k]).mean(axis=1)
-            col_array[colmiss_nan_placeholder] = colmiss_fill
-            col = col_array
-
-        else:
-            print('No or all missing entries')
-            col = col
-
-    else:
-        print('Not numeric type')
-        col = col
-
-    return col
+    return col_ndarray
 
 
-def impute_median_sd_2(df, k, outvar):
-    """Impute the missing values with the median, plus/ minus some share of the
-    standard deviation of the specific variable.
+def impute_kNN_2(df, col_name):
+    """Impute the missing values with the average of the k nearest neightbors.
+
+    Args:
+
+    df (pd.DataFrame() - data set
+    col_name (list) - names of variables which should be imputed
+
+    Returns:
+
+    col_ndarray_inverse () - imputed nd array
 
     """
+    col_ndarray = df[col_name].values
+    col_ndarray_normlized = scaler.fit_transform(col_ndarray)
+    col_ndarray_imputed = imputer_kNN.fit_transform(col_ndarray_normlized)
+    col_ndarray_inverse = scaler.inverse_transform(col_ndarray_imputed)
 
-    colmiss_name = df[outvar].select_dtypes(np.number).columns.tolist()
-    colmiss_ndarray = df[name].values
-
-    colmiss_len = np.arange(np.size(colmiss_ndarray, 1))
-    colmiss_median = np.nanmedian(colmiss_ndarray, axis = 0)
-    colmiss_sd = np.nanstd(colmiss_ndarray, axis=0)
-
-    for i in colmiss_len:
-        colmiss_nan_placeholder = np.isnan(colmiss_ndarray[:, i])
-        colmiss_fill = np.random.normal(colmiss_median[i], colmiss_sd[i], [colmiss_nan_placeholder.sum(), k]).mean(axis=1)
-        colmiss_ndarray[:,i][colmiss_nan_placeholder] = colmiss_fill
-
-    df_filled = df
-    df_filled[name] = colmiss_ndarray
-
-    return df_filled
+    return col_ndarray_inverse
