7382f80bacf8f553111225d5202eb048efbe0908
Adelina Garamow <adelina.garamow@outlook.de>
2020-03-06 11:57:35 +0100

    Add testing for msd imputer.

--- src/method_define/impute_method_test.py
+++ src/method_define/impute_method_test.py
@@ -17,22 +17,20 @@ np.random.seed(42)
 
 
 @pytest.fixture
-def setup_imputation():
+def setup_imputation_kNN():
     out = {}
 
     # case1: Standard case and k = 1.
     case1 = {
         "df": pd.DataFrame(
-            np.array(
-                [
-                    [nan, 6, nan, 1, nan],
-                    [4, 8, 5, 3, 1],
-                    [nan, 0, 1, 0, 1],
-                    [1, nan, 3, 0, nan],
-                    [3, nan, 2, 0, 4],
-                    [nan, 2, 3, 1, 0],
-                ]
-            ),
+            [
+                [nan, 6, nan, 1, nan],
+                [4, 8, 5, 3, 1],
+                [nan, 0, 1, 0, 1],
+                [1, nan, 3, 0, nan],
+                [3, nan, 2, 0, 4],
+                [nan, 2, 3, 1, 0],
+            ],
             columns=["A", "B", "C", "D", "E"],
         ),
         "knn": 1,
@@ -43,19 +41,17 @@ def setup_imputation():
     # case2: Standard case with k = 2.
     case2 = {
         "df": pd.DataFrame(
-            np.array(
-                [
-                    [nan, 3, 4, 1, nan],
-                    [4, nan, 5, 1, 1],
-                    [nan, 0, 1, 0, 1],
-                    [1, nan, 3, 0, nan],
-                    [3, nan, 2, 0, 4],
-                    [nan, 2, 3, 1, 0],
-                    [3, 2, nan, 0, 8],
-                    [6, 6, 3, 0, 2],
-                    [nan, 4, 3, 1, 3],
-                ]
-            ),
+            [
+                [nan, 3, 4, 1, nan],
+                [4, nan, 5, 1, 1],
+                [nan, 0, 1, 0, 1],
+                [1, nan, 3, 0, nan],
+                [3, nan, 2, 0, 4],
+                [nan, 2, 3, 1, 0],
+                [3, 2, nan, 0, 8],
+                [6, 6, 3, 0, 2],
+                [nan, 4, 3, 1, 3],
+            ],
             columns=["A", "B", "C", "D", "E"],
         ),
         "knn": 2,
@@ -66,16 +62,14 @@ def setup_imputation():
     # case3: One row with all entries missing.
     case3 = {
         "df": pd.DataFrame(
-            np.array(
-                [
-                    [nan, nan, nan, nan, nan],
-                    [4, 8, 5, 3, 1],
-                    [nan, 0, 1, 0, 1],
-                    [1, nan, 3, 0, nan],
-                    [3, nan, 2, 0, 4],
-                    [nan, 2, 3, 1, 0],
-                ]
-            ),
+            [
+                [nan, nan, nan, nan, nan],
+                [4, 8, 5, 3, 1],
+                [nan, 0, 1, 0, 1],
+                [1, nan, 3, 0, nan],
+                [3, nan, 2, 0, 4],
+                [nan, 2, 3, 1, 0],
+            ],
             columns=["A", "B", "C", "D", "E"],
         ),
         "knn": 1,
@@ -87,7 +81,7 @@ def setup_imputation():
     # same distance to all other columns.
     case4 = {
         "df": pd.DataFrame(
-            np.array([[nan, 6, nan, nan, nan], [4, 6, 5, 1, 1], [3, 6, 1, 0, 1]]),
+            [[nan, 6, nan, nan, nan], [4, 6, 5, 1, 1], [3, 6, 1, 0, 1]],
             columns=["A", "B", "C", "D", "E"],
         ),
         "knn": 1,
@@ -99,21 +93,19 @@ def setup_imputation():
 
 
 @pytest.fixture
-def expected_imputation():
+def expected_imputation_kNN():
     out = {}
 
     case1 = {
         "df": pd.DataFrame(
-            np.array(
-                [
-                    [3, 6, 3, 1, 0],
-                    [4, 8, 5, 3, 1],
-                    [1, 0, 1, 0, 1],
-                    [1, 2, 3, 0, 0],
-                    [3, 6, 2, 0, 4],
-                    [1, 2, 3, 1, 0],
-                ]
-            ),
+            [
+                [3, 6, 3, 1, 0],
+                [4, 8, 5, 3, 1],
+                [1, 0, 1, 0, 1],
+                [1, 2, 3, 0, 0],
+                [3, 6, 2, 0, 4],
+                [1, 2, 3, 1, 0],
+            ],
             columns=["A", "B", "C", "D", "E"],
         )
     }
@@ -121,19 +113,17 @@ def expected_imputation():
 
     case2 = {
         "df": pd.DataFrame(
-            np.array(
-                [
-                    [3.5, 3, 4, 1, 1.5],
-                    [4, 2.5, 5, 1, 1],
-                    [2, 0, 1, 0, 1],
-                    [1, 1, 3, 0, 6],
-                    [3, 1, 2, 0, 4],
-                    [2.5, 2, 3, 1, 0],
-                    [3, 2, 2.5, 0, 8],
-                    [6, 6, 3, 0, 2],
-                    [5, 4, 3, 1, 3],
-                ]
-            ),
+            [
+                [3.5, 3, 4, 1, 1.5],
+                [4, 2.5, 5, 1, 1],
+                [2, 0, 1, 0, 1],
+                [1, 1, 3, 0, 6],
+                [3, 1, 2, 0, 4],
+                [2.5, 2, 3, 1, 0],
+                [3, 2, 2.5, 0, 8],
+                [6, 6, 3, 0, 2],
+                [5, 4, 3, 1, 3],
+            ],
             columns=["A", "B", "C", "D", "E"],
         )
     }
@@ -141,16 +131,14 @@ def expected_imputation():
 
     case3 = {
         "df": pd.DataFrame(
-            np.array(
-                [
-                    [2.666667, 3.333333, 2.8, 0.8, 1.5],
-                    [4, 8, 5, 3, 1],
-                    [1, 0, 1, 0, 1],
-                    [1, 2, 3, 0, 0],
-                    [3, 0, 2, 0, 4],
-                    [1, 2, 3, 1, 0],
-                ]
-            ),
+            [
+                [2.666667, 3.333333, 2.8, 0.8, 1.5],
+                [4, 8, 5, 3, 1],
+                [1, 0, 1, 0, 1],
+                [1, 2, 3, 0, 0],
+                [3, 0, 2, 0, 4],
+                [1, 2, 3, 1, 0],
+            ],
             columns=["A", "B", "C", "D", "E"],
         )
     }
@@ -167,16 +155,64 @@ def expected_imputation():
     return out
 
 
-def test_kNN(setup_imputation, expected_imputation):
-    """Test if the imputation of kNN imputer with k=1 is correct for standard
-    case.
+def test_imputer_kNN(setup_imputation_kNN, expected_imputation_kNN):
+    """Test the imputation of kNN imputer.
 
     """
-    for case, expected_outcome in expected_imputation.items():
-        calc_imputation = setup_imputation[case]["df"]
-        calc_imputation[setup_imputation[case]["col_name"]] = impute_kNN(
-            **setup_imputation[case]
+    for case, expected_outcome in expected_imputation_kNN.items():
+        calc_imputation = setup_imputation_kNN[case]["df"]
+        calc_imputation[setup_imputation_kNN[case]["col_name"]] = impute_kNN(
+            **setup_imputation_kNN[case]
         )
         calc_imputation = calc_imputation.round(10)
 
         assert_frame_equal(calc_imputation, expected_outcome["df"], check_dtype=False)
+
+
+@pytest.fixture
+def setup_imputation_msd():
+    out = {}
+
+    # case1: Imputing missings from a normal distribution around the mean with
+    # the respective standard deviation times 0.25, of each column respectively.
+    case1 = {
+        "df": pd.DataFrame([[1, 2, np.nan], [np.nan, 3, 4], [5, np.nan, 6]]),
+    }
+    out["case1"] = case1
+
+    return out
+
+
+@pytest.fixture
+def expected_imputation_msd():
+    out = {}
+
+    case1 = {
+        "df": pd.DataFrame([[1, 2, 5], [3, 3, 4], [5, 2.5, 5]]),
+    }
+    out["case1"] = case1
+
+    return out
+
+
+def test_imputer_msd(setup_imputation_msd, expected_imputation_msd):
+    """Test the imputation of msd imputer.
+
+    """
+
+    df_imputed = expected_imputation_msd["case1"]["df"]
+    df_imputed_obs = df_imputed[setup_imputation_msd["case1"]["df"].isna()]
+
+    df_to_test = []
+    for i, j in zip(
+        df_imputed_obs.items(), setup_imputation_msd["case1"]["df"].items()
+    ):
+        col_to_test = (i[1] <= j[1].median() + 0.25 * j[1].std()) & (
+            i[1] >= j[1].median() - 0.25 * j[1].std()
+        )
+        df_to_test.append(col_to_test)
+
+    expected_outcome_msd = pd.DataFrame(df_to_test).T
+    calc_imputation_msd = pd.DataFrame(df_imputed_obs.notnull())
+
+    assert_frame_equal(calc_imputation_msd, expected_outcome_msd, check_dtype=False)
